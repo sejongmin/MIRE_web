d932f61a425f7519398ddec704dde17a64c7e3e4
OhJongMin <jmoh7480@gmail.com>
2023-02-05 15:49:09 +0900

    Add contact

---
+++ JS/contact.js
@@ -0,0 +1,6 @@
+function showDetail_jm(){
+    document.querySelector('#detail_jm').style.display="block";
+}
+function HideDetail_jm(){
+    document.querySelector('#detail_jm').style.display="none";
+}
