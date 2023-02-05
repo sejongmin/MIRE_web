9995bed393f85af3e067080b11e79d5464d9df54
OhJongMin <jmoh7480@gmail.com>
2023-02-06 02:25:11 +0900

    Add contact profile

--- JS/contact.js
+++ JS/contact.js
@@ -1,6 +1,27 @@
 function showDetail_jm(){
-    document.querySelector('#detail_jm').style.display="block";
+    document.querySelector('.jm').style.display="block";
 }
-function HideDetail_jm(){
-    document.querySelector('#detail_jm').style.display="none";
+function hideDetail_jm(){
+    document.querySelector('.jm').style.display="none";
+}
+
+function showDetail_gc(){
+    document.querySelector('.gc').style.display="block";
+}
+function hideDetail_gc(){
+    document.querySelector('.gc').style.display="none";
+}
+
+function showDetail_pt(){
+    document.querySelector('.pt').style.display="block";
+}
+function hideDetail_pt(){
+    document.querySelector('.pt').style.display="none";
+}
+
+function showDetail_gr(){
+    document.querySelector('.gr').style.display="block";
+}
+function hideDetail_gr(){
+    document.querySelector('.gr').style.display="none";
 }
