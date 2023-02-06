6762f917bba3cfa221db075ca50767a35ebdd4a1
OhJongMin <jmoh7480@gmail.com>
2023-02-06 18:05:54 +0900

    Edit overall

--- JS/contact.js
+++ JS/contact.js
@@ -25,3 +25,29 @@ function showDetail_gr(){
 function hideDetail_gr(){
     document.querySelector('.gr').style.display="none";
 }
+
+
+function showDetail_yj(){
+    document.querySelector('.yj').style.display="block";
+}
+function hideDetail_yj(){
+    document.querySelector('.yj').style.display="none";
+}
+
+function showDetail_sj(){
+    document.querySelector('.sj').style.display="block";
+}
+function hideDetail_sj(){
+    document.querySelector('.sj').style.display="none";
+}
+
+function showDetail_hs(){
+    document.querySelector('.hs').style.display="block";
+}
+function hideDetail_hs(){
+    document.querySelector('.hs').style.display="none";
+}
+
+let map = document.getElementsById("map");
+map.src = "https://www.openstreetmap.org/export/embed.html?bbox=" + (lon-0.01) + 
+				"%2C" + (lat-0.01) + "%2C" + (lon+0.01) + "%2C" + (lat + 0.01);
