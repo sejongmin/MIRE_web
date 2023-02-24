27e7f12c778b85197bad4d5b796bef24c7b1fdd3
OhJongMin <jmoh7480@gmail.com>
2023-02-24 16:16:33 +0900

    Edit home intro, contact button

---
+++ JS/home.js
@@ -0,0 +1,29 @@
+var docuid = function(id) {
+    return document.getElementById(id);
+};
+
+var inc = 0;
+var out = 0;
+var str = 'MI:RE is always loading...';
+var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var t;
+
+var anim = function() {
+    inc = (inc + 1) % 7;
+    if (inc === 0 && out < str.length) {
+        docuid('anim').appendChild(document.createTextNode(str[out]));
+        out++;
+    } else if (out >= str.length) {
+        docuid('shuffle').innerHTML = '';
+        removeInterval(t);
+    }
+    docuid('shuffle').innerHTML =
+      chars[Math.floor(Math.random() * chars.length)];
+};
+
+t = setInterval(anim, 45);
+docuid('anim').innerHTML = '';
+
+setTimeout(function(){
+    clearInterval(t);
+}, 8500);
